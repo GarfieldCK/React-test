4b7c15bb6fb7de27c0c888564db7db3772cfc681
GarfieldCK <chonlasit.kkaew@gmail.com>
2024-01-22 21:47:45 +0700

    show user data as a plain text

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
 
+interface userMetaData {
+  first_name: string,
+  last_name: string,
+  username: string
+}
+
 function App() {
+
+  // Fetch data:
+  const Fetch = () => {
+    const [userMetaData, setUserMetaData] = useState<userMetaData[]>([]);
+    useEffect(() => {
+      fetch("https://random-data-api.com/api/users/random_user?size=5")
+      .then((res) => {
+        return res.json();
+      })
+      .then((metaData) => {
+        console.log(metaData);
+        setUserMetaData(metaData);
+      });
+    }, []);
+    return <div>
+      <ul>
+        {userMetaData.map((user, index) => (
+          <li key={index}>
+            {user.first_name}
+            {user.last_name}
+            {user.username}
+          </li>
+        ))}
+      </ul>
+    </div>
+  }
   return (
     <div>
     <h1> Hello world</h1>
     <FetchButton/>
+    <Fetch/>
   </div>
   );
 }
